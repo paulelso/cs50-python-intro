0a869e6b82e781fc5ca084a6e11e3b91d6d7c0cc
psousa@twilio.com <paulelso@hotmail.com>
2023-12-05 19:23:14 +0100

    Committed improved twttr remove_vowels func which uses the str.join() method.

--- problem_set_2/twttr/twttr.py
+++ problem_set_2/twttr/twttr.py
@@ -1,16 +1,11 @@
 def main():
-    user_input = input()
-    output = remove_vowels(user_input)
-    print(output)
+    text = input("Input: ")
+    modified_text = remove_vowels(text)
+    print(modified_text)
 
-def remove_vowels(text):
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    text_without_vowels = ''
-    for char in text:
-        if char.lower() not in vowels:
-            text_without_vowels += char
-
-    return text_without_vowels
+def remove_vowels(s):
+    vowels = "aeiouAEIOU"
+    return ''.join(char for char in s if char not in vowels)
 
 if __name__ == "__main__":
     main()
