ce8600844705d528831e43663b56015f8efb4046
psousa@twilio.com <paulelso@hotmail.com>
2024-01-15 14:45:05 +0100

    Committed meows_argparse.py

--- problem_set_9/lesson/meows_argparse.py
+++ problem_set_9/lesson/meows_argparse.py
@@ -1,10 +1,10 @@
-import sys
+# python meows_argparse.py --help or -h
+# the user will be presented with some clues about how to use the program.
+import argparse
 
-if len(sys.argv) == 1:
+parser = argparse.ArgumentParser(description="Meow like a cat")
+parser.add_argument("-n", default=1, help="number of times to meow", type=int)
+args = parser.parse_args()
+
+for _ in range(args.n):
     print("meow")
-elif len(sys.argv) == 3 and sys.argv[1] == "-n":
-    n = int(sys.argv[2])
-    for _ in range(n):
-        print("meow")
-else:
-    print("usage: meows.py [-n Number]")
